516c280f796b3f00a06a0226ffd17984fad3804b
Jacob Chung <chungjac@msu.edu>
2023-04-09 02:56:06 -0400

    Added screen for launching a new game session, as well as finished the tutorial screen.

--- src/Game.java
+++ src/Game.java
@@ -6,17 +6,23 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class Game extends Application
@@ -24,6 +30,9 @@ public class Game extends Application
     // Create buttons for playing the game and viewing the tutorial.
     private Button btTutorial = new Button("Tutorial");
     private Button btPlay = new Button("Play");
+
+    public static final String MEDIA_URL = 
+        "https://www.wikihow.com/video/6/60/Play+Solitaire+Step+0+Version+3.360p.mp4";
 
     @Override // Override the start method in the Application class.
     public void start(Stage primaryStage) 
@@ -72,6 +81,73 @@ public class Game extends Application
         primaryStage.setTitle("Javataire"); // Set title.
         primaryStage.setScene(scene); // Place the scene in the stage.
         primaryStage.show(); // Display the stage.
+
+        btTutorial.setOnAction(e -> displayHelp());
+        btPlay.setOnAction(e -> playGame(primaryStage));
+    }
+
+    /**
+     * Handles event where "Help" button is clicked.
+     */
+    public void displayHelp()
+    {                        
+        Stage helpStage = new Stage();
+
+        Media media = new Media(MEDIA_URL);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+        Button playButton = new Button(">");
+        playButton.setOnAction(e -> {
+        if (playButton.getText().equals(">")) {
+            mediaPlayer.play();
+            playButton.setText("||");
+        } else {
+            mediaPlayer.pause();
+            playButton.setText(">");
+        }
+        });
+
+        Button rewindButton = new Button("<<");
+        rewindButton.setOnAction(e -> mediaPlayer.seek(Duration.ZERO));
+        
+        Slider slVolume = new Slider();
+        slVolume.setPrefWidth(150);
+        slVolume.setMaxWidth(Region.USE_PREF_SIZE);
+        slVolume.setMinWidth(30);
+        slVolume.setValue(50);
+        mediaPlayer.volumeProperty().bind(
+        slVolume.valueProperty().divide(100));
+
+        HBox hBox = new HBox(10);
+        hBox.setAlignment(Pos.CENTER);
+        hBox.getChildren().addAll(playButton, rewindButton,
+        new Label("Volume"), slVolume);
+
+        BorderPane pane = new BorderPane();
+        pane.setCenter(mediaView);
+        pane.setBottom(hBox);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 700, 600);
+        helpStage.setTitle("Tutorial"); // Set the stage title
+        helpStage.setScene(scene); // Place the scene in the stage
+        helpStage.show(); // Display the stage    
+    }
+
+    /**
+     * Handles event for launching game when "Play" button is pressed.
+     */
+    public void playGame(Stage primaryStage)
+    {
+        Stage playStage = new Stage();
+
+        // Create a scene and place it in the stage
+        // Scene scene = new Scene(pane, 800, 600);
+        playStage.setTitle("Current Session"); // Set the stage title
+        // primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.hide();
+        playStage.show(); // Display the stage 
     }
     public static void main(String[] args)
     {
